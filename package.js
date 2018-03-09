3430d9475a12e8778a0054eb42e0995f094e35ca
Pyrax <piranhadev@gmail.com>
2018-03-09 12:53:24 +0100

    Publish package

--- package.js
+++ package.js
@@ -1,17 +1,19 @@
 Package.describe({
   name: 'pyrax:meteor-accounts-deezer',
   version: '1.0.0',
-  summary: '',
+  summary: 'Login service for deezer',
   git: 'https://github.com/pyrax/meteor-accounts-deezer.git'
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.6.1');
+
   api.use('ecmascript');
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
 
-  api.use('pyrax:meteor-deezer-oauth');
+  api.use('pyrax:meteor-deezer-oauth@1.0.0');
   api.imply('pyrax:meteor-deezer-oauth');
 
   api.addFiles('deezer.js');
